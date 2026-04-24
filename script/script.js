a78fe289544426e672dcb9233fc2d47bfb7e6972
Joao A <joaobarao1803@gmail.com>
2026-04-24 15:05:00 -0300

    Arrumando um pouco JS

--- script/script.js
+++ script/script.js
@@ -2,33 +2,17 @@ function salvarCarros(event) {
     // Impedir  de recarregar a página
     event.preventDefault();
 
-    let titulo = document.getElementById("titulo");
-    let preco = document.getElementById("preco");
-    let marca = document.getElementById("marca");
-    let modelo = document.getElementById("modelo");
+    let titulo = document.getElementById("title").value;
+    let preco = document.getElementById("preco").value;
+    let marca = document.getElementById("marca").value;
+    let modelo = document.getElementById("modelo").value;
 
     let cambioSelecionado = document.querySelector("input[name='marcha']:checked");
 
-    let cambio = campoSelecionado ? cambioSelecionado.id : "Não informado";
+    let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
 
     let lista = document.getElementById("listarCarros");
     
-    let cartao = document.createElement("div")
-
-    cartao.style.border = "1px solid #ccc";
-    cartao.style.padding = "10px";
-    cartao.style.marginBottom = "10px";
-
-    cartao.innerHTML = '
-    <h3>${titulo}</h3>
-    <p><strong>Preço:</strong> R$ ${preco}</p>
-    <p><strong>Marca:</strong> R$ ${marca}</p>
-    <p><strong>Modelo:</strong> R$ ${modelo}</p>
-    <p><strong>Câmbio:</strong> R$ ${cambio}</p>
-    ';
-
-    lista.appendChild(cartao);
-
     let carro = {
         titulo,
         preco,
@@ -37,7 +21,11 @@ function salvarCarros(event) {
         cambio
     };
 
-    let carros = JSON.parse(localStorage.getItem("carro") || []);
+    let carros = JSON.parse(localStorage.getItem("carros") || []);
     carros.push(carro);
     localStorage.setItem("carros", JSON.stringify(carros));
+
+    adicionarNaTela(carro);
+
+    document.querySelector("form").reset()
 }
